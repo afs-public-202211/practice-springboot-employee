72b6723eb3c4481967e89253e49ceeae01f78773
Marie Chow <chowma3@oocl.com>
2022-12-06 17:04:12 +0800

    refactor: add NoEmployeeFoundException

--- src/main/java/com/afs/employee/EmployeeRepository.java
+++ src/main/java/com/afs/employee/EmployeeRepository.java
@@ -24,7 +24,7 @@ public class EmployeeRepository {
         return employeeList.stream()
                 .filter(employee -> employee.getId() == id)
                 .findFirst()
-                .get();
+                .orElseThrow(NoEmployeeFoundException::new);
     }
 
     public List<Employee> getEmployeeByGender(String gender) {
